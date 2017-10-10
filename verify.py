c27ae4aecf01c8786f4a4fed42d3c42db66fcd40
Max Shinn <max@maxshinnpotential.com>
2017-10-10 15:11:40 -0400

    Better documentation and code organization

--- verify.py
+++ verify.py
@@ -11,6 +11,16 @@ from exceptions import *
 _FUN_PROPS = "__verify__"
 
 def TypeFactory(v):
+    """Ensure `v` is a valid Type.
+
+    This function is used to convert user-specified types into
+    internal types for the verification engine.  It allows Type
+    subclasses, Type subclass instances, Python type, and user-defined
+    classes to be passed.  Returns an instance of the type of `v`.
+
+    Users should never access this function directly.
+    """
+
     if issubclass(type(v), Type):
         return v
     elif issubclass(v, Type):
@@ -205,6 +215,16 @@ class Or(Type):
         return [e for t in self.types for e in t.generate()]
 
 def has_fun_prop(f, k):
+    """Test whether function `f` has property `k`.
+
+    We define properties as annotations added to a function throughout
+    the process of defining a function for verification, e.g. the
+    argument types.  If `f` is an unannotated function, this returns
+    False.  If `f` has the property named `k`, it returns True.
+    Otherwise, it returns False.
+
+    Users should never access this function directly.
+    """
     if not hasattr(f, _FUN_PROPS):
         return False
     if not isinstance(getattr(f, _FUN_PROPS), dict):
@@ -214,54 +234,38 @@ def has_fun_prop(f, k):
     return True
 
 def get_fun_prop(f, k):
+    """Get the value of property `k` from function `f`.
+
+    We define properties as annotations added to a function throughout
+    the process of defining a function for verification, e.g. the
+    argument types.  If `f` does not have a property named `k`, this
+    throws an error.  If `f` has the property named `k`, it returns
+    the value of it.
+
+    Users should never access this function directly.
+    """
     if not has_fun_prop(f, k):
         raise InternalError("Function %s has no property %s" % (str(f), k))
     return getattr(f, _FUN_PROPS)[k]
 
 def set_fun_prop(f, k, v):
+    """Set the value of property `k` to be `v` in function `f`.
+
+    We define properties as annotations added to a function throughout
+    the process of defining a function for verification, e.g. the
+    argument types.  This sets function `f`'s property named `k` to be
+    value `v`.
+
+    Users should never access this function directly.
+    """
     if not hasattr(f, _FUN_PROPS):
         setattr(f, _FUN_PROPS, {})
     if not isinstance(getattr(f, _FUN_PROPS), dict):
         raise InternalError("Invalid function properties dictionary for %s" % str(f))
     getattr(f, _FUN_PROPS)[k] = v
 
-def _decorator(func, argtypes=None, kwargtypes=None, returntype=None, requires=None, ensures=None):
-    # @accepts decorator
-    if argtypes is not None:
-        argtypes = [TypeFactory(a) for a in argtypes]
-        if has_fun_prop(func, "argtypes"):
-            raise ValueError("Cannot set argument types twice")
-        set_fun_prop(func, "argtypes", argtypes)
-    if kwargtypes is not None:
-        kwargtypes = {k : TypeFactory(a) for k,a in kwargtypes.items()}
-        if has_fun_prop(func, "kwargtypes"):
-            raise ValueError("Cannot set set keyword argument types twice")
-        set_fun_prop(func, "kwargtypes", kwargtypes)
-    # @returns decorator
-    if returntype is not None:
-        returntype = TypeFactory(returntype)
-        if has_fun_prop(func, "returntype"):
-            raise ValueError("Cannot set return type twice")
-        set_fun_prop(func, "returntype", returntype)
-    # @requires decorator
-    if requires is not None:
-        if has_fun_prop(func, "requires"):
-            if not isinstance(get_fun_prop(func, "requires"), list):
-                raise InternalError("Invalid requires structure")
-            base_requires = get_fun_prop(func, "requires")
-        else:
-            base_requires = []
-        set_fun_prop(func, "requires", [requires]+base_requires)
-    # @ensures decorator
-    if ensures is not None:
-        if has_fun_prop(func, "ensures"):
-            if not isinstance(get_fun_prop(func, "ensures"), list):
-                raise InternalError("Invalid ensures strucutre")
-            base_ensures = get_fun_prop(func, "ensures")
-        else:
-            base_ensures = []
-        set_fun_prop(func, "ensures", [ensures]+base_ensures)
-    def decorated(*args, **kwargs):
+def _wrap(func):
+    def _decorated(*args, **kwargs):
         # @accepts decorator
         if has_fun_prop(func, "argtypes") and has_fun_prop(func, "kwargtypes"):
             argtypes = get_fun_prop(func, "argtypes")
@@ -345,51 +349,150 @@ def _decorator(func, argtypes=None, kwargtypes=None, returntype=None, requires=N
                 elif not eval(e, globals(), limited_locals):
                     raise ExitConditionsError("Ensures statement '%s' failed" % ensurement)
         return returnvalue
-    # We have already wrapped this function
     if has_fun_prop(func, "active"):
         return func
     else:
         set_fun_prop(func, "active", True)
         assign = functools.WRAPPER_ASSIGNMENTS + (_FUN_PROPS,)
-        wrapped = functools.wraps(func, assigned=assign)(decorated)
-        global __ALL_FUNCTIONS
-        __ALL_FUNCTIONS.append(wrapped)
+        wrapped = functools.wraps(func, assigned=assign)(_decorated)
+        if "__ALL_FUNCTIONS" in globals().keys():
+            global __ALL_FUNCTIONS
+            __ALL_FUNCTIONS.append(wrapped)
         return wrapped
+    
 
 def accepts(*argtypes, **kwargtypes):
-    return lambda f : _decorator(f, argtypes=argtypes, kwargtypes=kwargtypes)
+    argtypes = [TypeFactory(a) for a in argtypes]
+    kwargtypes = {k : TypeFactory(a) for k,a in kwargtypes.items()}
+    def _decorator(func):
+        # @accepts decorator
+        if argtypes is not None:
+            if has_fun_prop(func, "argtypes"):
+                raise ValueError("Cannot set argument types twice")
+            set_fun_prop(func, "argtypes", argtypes)
+        if kwargtypes is not None:
+            if has_fun_prop(func, "kwargtypes"):
+                raise ValueError("Cannot set set keyword argument types twice")
+            set_fun_prop(func, "kwargtypes", kwargtypes)
+        return _wrap(func)
+    return _decorator
+
 def returns(returntype):
-    return lambda f : _decorator(f, returntype=returntype)
+    returntype = TypeFactory(returntype)
+    def _decorator(func):
+        # @returns decorator
+        if has_fun_prop(func, "returntype"):
+            raise ValueError("Cannot set return type twice")
+        set_fun_prop(func, "returntype", returntype)
+        return _wrap(func)
+    return _decorator
+            
 def requires(condition):
-    return lambda f : _decorator(f, requires=condition)
+    def _decorator(func):
+        # @requires decorator
+        if has_fun_prop(func, "requires"):
+            if not isinstance(get_fun_prop(func, "requires"), list):
+                raise InternalError("Invalid requires structure")
+            base_requires = get_fun_prop(func, "requires")
+        else:
+            base_requires = []
+        set_fun_prop(func, "requires", [condition]+base_requires)
+        return _wrap(func)
+    return _decorator
+
 def ensures(condition):
-    return lambda f : _decorator(f, ensures=condition)
+    def _decorator(func):
+    # @ensures decorator
+        if has_fun_prop(func, "ensures"):
+            if not isinstance(get_fun_prop(func, "ensures"), list):
+                raise InternalError("Invalid ensures strucutre")
+            base_ensures = get_fun_prop(func, "ensures")
+        else:
+            base_ensures = []
+        set_fun_prop(func, "ensures", [condition]+base_ensures)
+        return _wrap(func)
+    return _decorator
 
 def test_function(func):
-    assert hasattr(func, _FUN_PROPS), "No argument annotations"
+    """Perform a unit test of a single function.
+
+    Create a series of test cases by finding the type of each of the
+    function arguments, and creating all possible combinations of test
+    cases for each type using the type's generate() function.  Then,
+    test the function with each input.  This is roughly equivalent to
+    just running the function with each input, because the exceptions
+    should be caught at runtime.
+
+    Assuming runtime checking is enabled (as it should be), running
+    this on a function only tests it for inputs it is likely to
+    encounter in the future.
+    """
+    # Ensure the user has specified argument types using the @accepts
+    # decorator.
+    assert has_fun_prop(func, "argtypes"), "No argument annotations"
+    # Generate all combinations of arguments as test cases.  Some
+    # argument types cannot be generated automatically.  If we
+    # encounter one of these, unit testing won't work.
     try:
         testcases = itertools.product(*[e.generate() for e in get_fun_prop(func, "argtypes")])
-        if not testcases:
-            print("Warning: %s could not be tested" % func.__name__)
-        for tc in testcases:
-            try:
-                func(*tc)
-            except EntryConditionsError:
-                continue
     except NoGeneratorError:
+        testcases = []
+    if not testcases:
         print("Warning: %s could not be tested" % func.__name__)
+        return
+    # If entry conditions are met, simply running the function will be
+    # enough of a test, since all values are checked at runtime.  So
+    # execute the function once for each combination of arguments.
+    for tc in testcases:
+        try:
+            func(*tc)
+        except EntryConditionsError:
+            continue
 
-if __name__ != "__main__":
-    __ALL_FUNCTIONS = []
-
+# If called as "python3 -m verify script_file_name.py", then unit test
+# script_file_name.py.  By this, we mean call the function
+# test_function on each function which has annotations.  Note that we
+# must execute the entire file script_file_name.py in order to do
+# this, so any time-intensive code should be in a __name__ ==
+# "__main__" guard, which will not be executed.
+#
+# We know if a function has annotations because all annotations are
+# passed through the _decorator function (so that we can ensure we
+# only use at most one more stack frame no matter how many function
+# annotations we have).  The _decorator function will look for the
+# global variable __ALL_FUNCTIONS to be defined within the verify
+# module.  If it is defined, it will add one copy of each decorated
+# function to the list.  Then, we can perform the unit test by
+# iterating through each of these.  This has the advantage of allowing
+# nested functions and object methods to be discovered for
+# verification.
 if __name__ == "__main__":
-    assert len(sys.argv) == 2, "Invalid argument, please pass a python file"
-    globs = {}
-    exec(open(sys.argv[1], "r").read()+"\n\nimport verify as __verify", globs)
-    all_functions = globs["__verify"].__ALL_FUNCTIONS
+    # Pass exactly one argement: the python file to check
+    if len(sys.argv) != 2: # len == 2 because the path to the module is arg 1.
+        exit("Invalid argument, please pass a python file")
+    # Add the __ALL_FUNCTIONS to the global scope of the verify module
+    # and then run the script.  Save the global variables from script
+    # execution so that we can find the __ALL_FUNCTIONS variable once
+    # the script has finished executing.
+    globs = {} # Global variables from script execution
+    prefix = "import verify as __verifymod\n__verifymod.__ALL_FUNCTIONS = []\n"
+    exec(prefix+open(sys.argv[1], "r").read(), globs)
+    all_functions = globs["__verifymod"].__ALL_FUNCTIONS
+    # Test each function from the script.
     for f in all_functions:
-        assert hasattr(f, '__verify__')
-        print("    Testing %s..." % f.__name__, end="")
+        assert hasattr(f, _FUN_PROPS), "Internal error"
+        # Some function executions might take a while, so we print to
+        # the screen when we begin testing a function, and then erase
+        # it after we finish testing the function.  This is like a
+        # make-shift progress bar.
+        start_text = "    Testing %s..." % f.__name__
+        print(start_text, end="", flush=True)
         test_function(f)
-        print("\b"*len("    Testing %s..." % f.__name__)+"    Tested %s    " % f.__name__)
-    print("Finished testing %i functions in %s." % (len(all_functions), sys.argv[1]))
+        import time
+        time.sleep(1)
+        # Extra spaces after "Tested %s" compensate for the fact that
+        # the string to signal the start of testing function f is
+        # longer than the string to signal function f has been tested,
+        # so this avoids leaving extra characters on the terminal.
+        print("\b"*len(start_text)+"    Tested %s    " % f.__name__, flush=True)
+    print("Tested %i functions in %s." % (len(all_functions), sys.argv[1]))
