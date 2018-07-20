d6ef291c86c844cc3951a4eea3d1bcb83fccfc41
Max Shinn <max@maxshinnpotential.com>
2018-07-20 16:30:44 -0400

    README in setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ from setuptools import setup
 
 from paranoid._version import __version__
 
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
 setup(
     name = 'paranoid-scientist',
     version = __version__,
     description = 'Runtime verification and automated testing for scientific code',
+    long_description = long_desc,
+    long_description_content_type='text/markdown',
     author = 'Max Shinn',
     author_email = 'maxwell.shinn@yale.edu',
     url = 'https://github.com/mwshinn/paranoidscientist',
