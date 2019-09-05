06f92426d72298345395d96a520221de46d7f0d4
Max Shinn <max@maxshinnpotential.com>
2019-09-05 08:21:43 -0400

    Better performance for numpy types

--- paranoid/types/numeric.py
+++ paranoid/types/numeric.py
@@ -20,8 +20,10 @@ except ImportError:
 class Numeric(Type):
     """Any integer or float, including inf, -inf, and nan."""
     def test(self, v):
-        super().test(v)
         assert isinstance(v, NUMERIC_TYPES), "Invalid numeric"
+    def test_numpy(self, v):
+        assert isinstance(v.dtype.type(), np.floating) or \
+            isinstance(v.dtype.type(), np.integer), "Invalid datatype"
     def generate(self):
         # Check infinity, nan, 0, +/- numbers, a float, a small/big number
         yield math.inf # Check infs
@@ -43,11 +45,15 @@ class Numeric(Type):
             yield np.float16(3.141)
             yield np.float64(.01)
 
-class ExtendedReal(Numeric):
+class ExtendedReal(Type):
     """Any integer or float, excluding nan."""
     def test(self, v):
-        super().test(v)
+        assert isinstance(v, NUMERIC_TYPES), "Invalid numeric"
         assert not math.isnan(v), "Number cannot be nan"
+    def test_numpy(self, v):
+        assert isinstance(v.dtype.type(), np.floating) or \
+            isinstance(v.dtype.type(), np.integer), "Invalid datatype"
+        assert not np.any(np.isnan(v)), "Number cannot be nan"
     def generate(self):
         # Check infinity, nan, 0, +/- numbers, a float, a small/big number
         yield math.inf # Check infs
@@ -67,13 +73,15 @@ class ExtendedReal(Numeric):
             yield np.float16(3.141)
             yield np.float64(.01)
 
-class Number(Numeric):
+class Number(Type):
     """Any integer or float, excluding inf, -inf, and nan."""
     def test(self, v):
-        super().test(v)
         assert isinstance(v, NUMERIC_TYPES), "Invalid number"
-        assert not math.isinf(v), "Number must be finite"
-        assert not math.isnan(v), "Number cannot be nan"
+        assert math.isfinite(v), "Number must not be nan or inf"
+    def test_numpy(self, v):
+        assert isinstance(v.dtype.type(), np.floating) or \
+            isinstance(v.dtype.type(), np.integer), "Invalid datatype"
+        assert np.all(np.isfinite(v)), "Number cannot be nan or inf"
     def generate(self):
         yield 0
         yield 1
@@ -89,11 +97,18 @@ class Number(Numeric):
             yield np.float64(.01)
             yield np.float64(10)
 
-class Integer(Number):
+class Integer(Type):
     """Any integer."""
     def test(self, v):
-        super().test(v)
+        assert isinstance(v, NUMERIC_TYPES), "Invalid number"
+        assert not math.isinf(v), "Number must be finite"
+        assert not math.isnan(v), "Number cannot be nan"
         assert v // 1 == v, "Invalid integer"
+    def test_numpy(self, v):
+        assert isinstance(v.dtype.type(), np.floating) or \
+            isinstance(v.dtype.type(), np.integer), "Invalid datatype"
+        assert np.all(np.isfinite(v)), "Number cannot be nan or inf"
+        assert np.all(v // 1 == v), "Invalid integer"
     def generate(self):
         yield -100
         yield -1
@@ -111,6 +126,9 @@ class Natural0(Integer):
     def test(self, v):
         super().test(v)
         assert v >= 0, "Must be greater than or equal to 0"
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v >= 0), "Must be greater than or equal to 0"
     def generate(self):
         yield 0
         yield 1
@@ -127,6 +145,9 @@ class Natural1(Integer):
     def test(self, v):
         super().test(v)
         assert v > 0, "Must be greater than 0"
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v > 0), "Must be greater than 0"
     def generate(self):
         yield 1
         yield 2
@@ -147,8 +168,7 @@ class Range(Number):
     """
     def __init__(self, low, high):
         super().__init__(low, high)
-        assert low in Numeric() and high in Numeric(), "Invalid bounds"
-        assert not (math.isnan(low) or math.isnan(high)), "Bounds can't be nan"
+        assert low in ExtendedReal() and high in ExtendedReal(), "Invalid bounds"
         assert low < high, \
             "Low %s must be strictly greater than high %s" % (low, high)
         assert not (math.isinf(low) and math.isinf(high)), \
@@ -159,6 +179,12 @@ class Range(Number):
         super().test(v)
         assert self.low <= v <= self.high, "Value %f must be greater" \
             "than %f and less than %f" % (v, self.low, self.high)
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(self.low <= v), "Values %f must be greater" \
+            "than %f" % (v, self.low)
+        assert np.all(v <= self.high), "Values %f must be less" \
+            "than %f" % (v, self.high)
     def generate(self):
         EPSILON = 1e-5
         if not math.isinf(self.low):
@@ -178,7 +204,10 @@ class RangeClosedOpen(Range):
     """A half open interval from `low` (closed) to `high` (open)."""
     def test(self, v):
         super().test(v)
-        assert v != self.high, "Value must be strictly greater than %f" % self.high
+        assert v != self.high, "Value must be strictly less than %f" % self.high
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v != self.high), "Values must be strictly less than %f" % self.high
     def generate(self):
         for v in super().generate():
             if v != self.high:
@@ -188,7 +217,10 @@ class RangeOpenClosed(Range):
     """A half open interval from `low` (open) to `high` (closed)."""
     def test(self, v):
         super().test(v)
-        assert v != self.low, "Value must be strictly less than %f" % self.low
+        assert v != self.low, "Value must be strictly greater than %f" % self.low
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v != self.low), "Values must be strictly greater than %f" % self.low
     def generate(self):
         for v in super().generate():
             if v != self.low:
@@ -198,8 +230,12 @@ class RangeOpen(Range):
     """Any number in the open interval from `low` to `high`."""
     def test(self, v):
         super().test(v)
-        assert v != self.low
-        assert v != self.high
+        assert v != self.low, "Value must be strictly greater than %f" % self.low
+        assert v != self.high, "Value must be strictly less than %f" % self.high
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v != self.low), "Value must be strictly greater than %f" % self.low
+        assert np.all(v != self.high), "Value must be strictly less than %f" % self.high
     def generate(self):
         for v in Range.generate(self):
             if not v in [self.low, self.high]:
@@ -209,7 +245,10 @@ class Positive0(Number):
     """A positive number, including zero."""
     def test(self, v):
         super().test(v)
-        assert v >= 0
+        assert v >= 0, "Value must be non-negative"
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v >= 0), "Values must be non-negative"
     def generate(self):
         yield 4.3445 # A float
         yield 1
@@ -220,7 +259,10 @@ class Positive(Number):
     """A positive number, excluding zero."""
     def test(self, v):
         super().test(v)
-        assert v > 0
+        assert v > 0, "Value must be positive"
+    def test_numpy(self, v):
+        super().test_numpy(v)
+        assert np.all(v > 0), "Values must be positive"
     def generate(self):
         yield 4.3445 # A float
         yield 1
@@ -232,32 +274,36 @@ class NDArray(Type):
         super().__init__(d=d, t=t)
         assert USE_NUMPY, "Numpy support not enabled"
         if d is not None:
-            assert (d in Integer())  and d>0
+            assert (d in Integer())  and d>0, "Invalid dimension"
         # TODO support non-numeric types
+        
+        # Create a function for testing whether values are an instance
+        # of the specified type.  If no type is specified, the
+        # function should always return true.  If a type is specified,
+        # check if it has the special "test_numpy" method.  If so, use
+        # that on the full array.  (This can substantially improve
+        # performance.)  If not, test each value individually.
         if t is not None:
             assert isinstance(TypeFactory(t), Type)
             self.type = TypeFactory(t)
+            if hasattr(self.type, "test_numpy"):
+                self.testfunc = self.type.test_numpy
+            else:
+                def testfunc(x):
+                    for xv in x.flatten():
+                        assert xv in self.type, \
+                            "Array value %s is not of type %s" % (xv, repr(self.type))
+                self.testfunc = testfunc
         else:
             self.type = None
+            self.testfunc = lambda x : True
         self.d = d
     def test(self, v):
         super().test(v)
         assert isinstance(v, np.ndarray), "V is not an NDArray, it is a " + str(type(v))
         if self.d is not None:
             assert len(v.shape) == self.d
-        if self.type is not None:
-            # Optimizing this for numbers, which are used often
-            if type(self.type) is Number:
-                assert np.all(np.isfinite(v))
-            elif type(self.type) is Positive:
-                assert np.all(np.isfinite(v))
-                assert np.all(v > 0)
-            elif type(self.type) is Positive0:
-                assert np.all(np.isfinite(v))
-                assert np.all(v >= 0)
-            else:
-                for fv in v.flatten():
-                    assert fv in self.type, "Array value %s is not of type %s" % (fv, repr(self.type))
+        self.testfunc(v)
     def generate(self):
         # TODO fix, and more of these
         if self.type:
